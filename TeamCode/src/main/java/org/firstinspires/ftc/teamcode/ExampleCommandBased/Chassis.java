03e0f387a553555c15f2c4fe5a09dff9595352fe
Angusbutinriverdale <anguszheng649@gmail.com>
2025-09-27 23:32:22 -0400

    I only made a basic chassis im only doing this because I want to add pedro files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleCommandBased/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleCommandBased/Chassis.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode.ExampleCommandBased;
 
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.drivebase.RobotDrive;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.arcrobotics.ftclib.geometry.Vector2d;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Chassis {
+
+Motor frontLeft = new Motor();
+Motor frontRight = new Motor();
+Motor backLeft = new Motor();
+Motor backRight = new Motor();
+MecanumDrive mecanumDrive = new MecanumDrive(true, frontLeft, frontRight,backLeft,backRight);
+
+
+
+public void fieldOriented(double strafeSpeed,double forwardSpeed, double turn, double heading) {
+    mecanumDrive.driveFieldCentric(strafeSpeed, forwardSpeed, turn, heading);
 }
+public void robotOriented(double strafeSpeed, double forwardSpeed, double turnSpeed) {
+    mecanumDrive.driveRobotCentric(strafeSpeed, forwardSpeed, turnSpeed);
+}
+
+
+
+//you need to figure out how to use auto! Allegedly it's pretty easy just check it out later
+    }
+
+
+
+
+
+
+
+
+
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleLinearCode/ExampleOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleLinearCode/ExampleOpMode_Linear.java
@@ -7,17 +7,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(group = "R3P2 Examples", name = "ExampleOpMode")
 public class ExampleOpMode_Linear extends LinearOpMode {
-    DcMotor shootMotor;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        shootMotor = hardwareMap.get(DcMotor.class, "ShootMotor");
+
         waitForStart();
 
         while(opModeIsActive()){
             if(gamepad1.a) {
-                shootMotor.setPower(1);
+
             } else{
-                shootMotor.setPower(0);
+
             }
         }
     }
